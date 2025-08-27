c0dcd5287da0a9f99eeb2759bba1a469314e11ff
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2025-08-27 03:10:37 +0200

    test

--- c_lib/mylib.h
+++ c_lib/mylib.h
@@ -1,14 +1,24 @@
 #ifndef MYLIB_H
 #define MYLIB_H
 
+#ifdef _WIN32
+  #ifdef MYLIB_EXPORTS
+    #define MYLIB_API __declspec(dllexport)
+  #else
+    #define MYLIB_API __declspec(dllimport)
+  #endif
+#else
+  #define MYLIB_API
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int add_numbers(int a, int b);
+MYLIB_API int add_numbers(int a, int b);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // MYLIB_H
+#endif
